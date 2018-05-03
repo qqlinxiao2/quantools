c7bc0c638010c15484635219c36493567a7ac6f6
yuchen <qqlinxiao2@163.com>
2018-05-03 22:36:57 +0800

    quanttool

--- imageprocess/matplotdemo.py
+++ imageprocess/matplotdemo.py
@@ -19,8 +19,11 @@ def loadTickCsvAsNumpy(fileName):
                         #comments='#', delimiter=',',usecols=(2, 3), unpack=True)
     reader = csv.DictReader(open(fileName, 'r',encoding='gbk'))
     for d in reader:
-        price.append(int(d['最新']))
-        date.append(datetime.strptime(d['时间'], '%Y-%m-%d %H:%M:%S.%f'))
+        p = int(d['最新'])
+        t = datetime.strptime(d['时间'], '%Y-%m-%d %H:%M:%S.%f')
+        if (t.hour >= 21 and t.hour <=23) or (t.hour >= 9 and t.hour <=15):
+            price.append(p)
+            date.append(t)
 
 def format_date(x, pos=None):
     #保证下标不越界,很重要,越界会导致最终plot坐标轴label无显示
@@ -49,14 +52,29 @@ def timeWindoAanalysis(timeSpace,scope):
     for i in range(len(date)):
         d = date[i]
         p = price[i]
-        if queue.== 0:
-            queue.
+        if queue.__len__() == 0:
+            queue.append(i)
+        else:
+            index = queue.popleft()
+            if date[i].timestamp() - date[index].timestamp() > timeSpace:
+                #有效时间窗 替换
+                queue.append(i)
+            else:
+                #无效时间窗 插入
+                queue.appendleft(index)
+                queue.append(i)
+
+
 
 
 if __name__ == '__main__':
-    #加载数据
-    loadTickCsvAsNumpy('P:\\Work\\quant\\m\\m主力连续_20180323.csv')
-    #分析
+    # #加载数据
+    # loadTickCsvAsNumpy('F:\BaiduNetdiskDownload\TICK数据\m\m主力连续_20180323.csv')
+    # #分析
+    # timeWindoAanalysis(60,5)
+    # #画图
+    # #drawImage(date,price)
 
-    #画图
-    drawImage(date,price)
+    # t = datetime.strptime('2018-03-22 21:00:00.379', '%Y-%m-%d %H:%M:%S.%f')
+    # t2 = datetime.strptime('2018-03-22 21:00:01.879', '%Y-%m-%d %H:%M:%S.%f')
+    # print(t2.timestamp()-t.timestamp())
